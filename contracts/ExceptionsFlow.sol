b215a8a66d449d64393b76450b49296d5b068d78
ungalexy <ungalexy@gmail.com>
2022-03-25 16:11:17 +0300

    comments

--- contracts/ExceptionsFlow.sol
+++ contracts/ExceptionsFlow.sol
@@ -3,23 +3,47 @@ pragma solidity >=0.5.13 <0.9.0; // !!! handler version just for local vscode
 
 /**
  * ExceptionsFlow
+ *
+ * - errors are "state reverting"
+ * - require, assert, revert
+ * - caching is not possoble within solidity
+ * - revert() and require() can return an error string
+ * - revert() and require() revert the transaction
+ * - revert() and require() are pretty much the same
+ * - require() reverting the operation & return the remaining gas to the caller
+ * - require() used to validate user input
+ * - assert() used to validated invariants - contract's values
+ * - best pratice is tm avoid assert(), use it only in specific cases which should betrated as exceptions
  */
 contract ExceptionsFlow {
     /**
-     * Gate with required condition
+     * Gate with REQUIRED condition
      */
-    function requiredGate() public view returns (uint256) {
-        require(1 > 2, "1 must be grather 2, otherwise the action will stop");
+    function requiredGate(uint256 handler) public returns (uint256) {
+        // require condition
+        require(handler > 2, "handler must be grather than 2");
         // do somrhting...
-        return 1;
+        return handler;
     }
 
     /**
-     * Gate with assertion condition
+     * Gate with REVERT condition
      */
-    function asssertionGate(uint256 handler) public view returns (uint256) {
+    function revertGate(uint256 handler) public returns (uint256) {
+        // require condition
+        if (handler < 2) {
+            revert("handler must be grather than 2");
+        }
+        // do somrhting...
+        return handler;
+    }
+
+    /**
+     * Gate with ASSEERT condition
+     */
+    function asssertionGate(uint256 handler) public returns (uint256) {
         // value must be grather than 1
-        require(handler > 1, "value must be grather 1");
+        require(handler > 1, "value must be grather than 1");
 
         // howevenr, the value can't be exacly 5
         assert(handler != 5);
