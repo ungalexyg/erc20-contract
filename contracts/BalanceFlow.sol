82ca9c0d5cc1c7d1ae85253734788915715e9399
ungalexy <ungalexy@gmail.com>
2022-03-25 12:34:24 +0300

    add option to grab all the balance

--- contracts/BalanceFlow.sol
+++ contracts/BalanceFlow.sol
@@ -23,7 +23,7 @@ contract BalanceFlow {
      * any function in Solidity with the modifier Payable ensures that the function can send and receive Ether.
      * it is required when values that reprresent money sent in, and going to be stored on contract's varaibles
      */
-    function sendMoney() public payable {
+    function sendMoneyToContract() public payable {
         //msg global object avauilble everywhere in the contract up on request
         balanceReceived += msg.value; // now the balanceReceived will store the value in the contract and will be avaible in next request
     }
@@ -32,7 +32,7 @@ contract BalanceFlow {
      * Get the current contrac's balance
      * display it in public up on request
      */
-    function getBalance() public view returns (uint256) {
+    function getContractBalance() public view returns (uint256) {
         // the contract's address available when using the casting address(this)
         // which returns the address object that has the balance property
         // balanceReceived
@@ -44,10 +44,49 @@ contract BalanceFlow {
      * unlike in sendMoney(), this function shouldn't be defined as payable since there is no incoming payment oprations
      * it is only transferring contract's balance to the current contract's caller
      */
-    function withdrwalMoney() public {
-        // define the aender as payable before taking payment operation
-        // address objects cab casted as payable payable(msg.sender);
+    function withdrwalAmountToSender(uint256 _amount) public {
+        // gate - make sure that the balance has the requested amount
+        require(_amount > this.getContractBalance());
+
+        // cascade the sender as payable before taking payment operation
         address payable to = payable(msg.sender);
-        to.transfer(this.getBalance());
+
+        // use the build in payable.transfer() method to send the amount to the address
+        to.transfer(_amount);
+
+        // decrease the amount from the balance
+        balanceReceived -= _amount;
+    }
+
+    /**
+     * Withdrwal from the contract's balance to a given address
+     * in this canse since the address came from input, it should be defined as payable
+     */
+    function withdrwalAmountToAddress(address payable _to, uint256 _amount)
+        public
+    {
+        // gate - make sure that the balance has the requested amount
+        require(_amount > this.getContractBalance());
+
+        // use the build in payable.transfer() method to send the amount to the address
+        _to.transfer(_amount);
+
+        // decrease the amount from the balance
+        balanceReceived -= _amount;
+    }
+
+    /**
+     * Withdrwal from the contract's balance to a given address
+     * in this canse since the address came from input, it should be defined as payable
+     */
+    function withdrwalFullBalanceToSender() public {
+        // use the build in payable.transfer() method to send the amount to the address
+        address payable to = payable(msg.sender);
+
+        // send all the balance to the sender
+        to.transfer(this.getContractBalance());
+
+        // reset the balance to 0
+        balanceReceived = 0;
     }
 }
